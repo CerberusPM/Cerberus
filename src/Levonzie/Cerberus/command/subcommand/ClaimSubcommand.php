8b758f5eaf3ae1c7625178d7a7efe2e708b500c3
Levonzie <44316602+Levonzie@users.noreply.github.com>
2023-10-07 14:26:18 +0300

    Implement claim subcommand
    On run, claim subcommand first checks if all positions are properly
    selected (both positions are selected and are in the same world),
    notifying user if something is incorrect. Then it checks whether land
    with given name already exists. Finally it checks if land intersects
    land claimed by somebody else, taking appropriate actions and allowing
    to claim if user has a bypass permission. Land claiming is done by
    using LandManager directly. There was an option to implement land
    creation functionality using createLand() CerberusAPI method, catching
    LandExistsException and LandIntersectException, but more
    straightforward way through LandManager is being used for now.
    
    Details:
    
    - Implement claim subcommand functionality
    
    - Add cerberus.command.claim.bypass_intersect permission to plugin.yml
      Users with this permission are able to create landclaims even if
      their selections intersect landclaims of other users
    
    - Add translation keys used by claim subcommand to English and Russian
      language files.
    
    - Bump dev language version

--- src/Levonzie/Cerberus/command/subcommand/ClaimSubcommand.php
+++ src/Levonzie/Cerberus/command/subcommand/ClaimSubcommand.php
@@ -27,14 +27,72 @@ use pocketmine\command\CommandSender;
 use CortexPE\Commando\BaseSubCommand;
 use CortexPE\Commando\args\RawStringArgument;
 
+use Levonzie\Cerberus\CerberusAPI;
+use Levonzie\Cerberus\utils\SelectionManager;
+use Levonzie\Cerberus\utils\ConfigManager;
+use Levonzie\Cerberus\utils\LangManager;
+use Levonzie\Cerberus\utils\LandManager;
+use Levonzie\Cerberus\Landclaim;
+
+use function is_null;
+
 class ClaimSubcommand extends BaseSubCommand {
     protected function prepare(): void {
         $this->registerArgument(0, new RawStringArgument("name")); //Name of a landclaim
         
         $this->setPermission("cerberus.command.claim");
+        
+        $this->api = CerberusAPI::getInstance();
+        $this->config_manager = ConfigManager::getInstance();
+        $this->lang_manager = LangManager::getInstance();
     }
     
     public function onRun(CommandSender $sender, string $alias, array $args): void {
-        //TODO
+        $selector = $sender->getName();
+        //Check if all positions are selected 
+        if (!SelectionManager::hasSelectedFirst($selector) && !SelectionManager::hasSelectedSecond($selector)) {
+            $sender->sendMessage($this->config_manager->getPrefix() . $this->lang_manager->translate("command.claim.select_both_positions"));
+            return;
+        }
+        elseif (!SelectionManager::hasSelectedFirst($selector)) {
+            $sender->sendMessage($this->config_manager->getPrefix() . $this->lang_manager->translate("command.claim.select_pos1"));
+            return;
+        }
+        elseif (!SelectionManager::hasSelectedSecond($selector)) {
+            $sender->sendMessage($this->config_manager->getPrefix() . $this->lang_manager->translate("command.claim.select_pos2"));
+            return;
+        }
+        else {
+            $pos1 = SelectionManager::getSelectedFirstPosition($selector);
+            $pos2 = SelectionManager::getSelectedSecondPosition($selector);
+        }
+        //Check if positions are located in the same world
+        if ($pos1[1] != $pos2[1]) {
+            $sender->sendMessage($this->config_manager->getPrefix() . $this->lang_manager->translate("command.claim.world_mismatch"));
+            return;
+        } else {
+            $world = $pos1[1];
+        }
+        //TODO: claim limit permission
+        //Check if land already exists
+        if ($this->api->landExists($args["name"])) {
+            $sender->sendMessage($this->config_manager->getPrefix() . $this->lang_manager->translate("command.claim.already_exists", [$args["name"]]));
+            return;
+        }
+        $new_land = new Landclaim($args["name"], $selector, $pos1[0], $pos2[0], $world);
+        //Check if intersects land owned by somebody else
+        $intersecting_land = $this->api->getIntersectingLand($new_land);
+        if (!is_null($intersecting_land) && $intersecting_land->getOwner() != $selector) {
+            if (!$sender->hasPermission("cerberus.command.claim.bypass_intersect")) {
+                $sender->sendMessage($this->config_manager->getPrefix() . $this->lang_manager->translate("command.claim.intersects", [$intersecting_land->getName(), $intersecting_land->getOwner()]));
+                return;
+            } else
+                $sender->sendMessage($this->config_manager->getPrefix() . $this->lang_manager->translate("command.claim.intersects_notification", [$intersecting_land->getName(), $intersecting_land->getOwner()]));
+        }
+        //Finally create a landclaim
+        LandManager::registerLandclaim($new_land);
+        SelectionManager::deselectAll($selector);
+        
+        $sender->sendMessage($this->config_manager->getPrefix() . $this->lang_manager->translate("command.claim.success", [$args["name"]]));
     }
 }
